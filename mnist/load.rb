6d8f295e2868269dfd104634a8c3ee6bc701e8eb
Hou Yuanjie <houyuanjie555@qq.com>
2024-12-17 09:03:21 +0800

    refactor(mnist): 重构 MNIST 数据加载逻辑

--- mnist/load.rb
+++ mnist/load.rb
@@ -1,3 +1,5 @@
+require 'matrix'
+
 module Mnist
   IMAGE_FILE_MAGIC_NUMBER = 0x0000_0803
   LABEL_FILE_MAGIC_NUMBER = 0x0000_0801
@@ -15,15 +17,20 @@ module Mnist
       magic_number = file.read(4).unpack1('N')
       raise ArgumentError, "File #{idx_file} is not an image file." unless magic_number == IMAGE_FILE_MAGIC_NUMBER
 
-      num_images = file.read(4).unpack1('N')
-      num_rows = file.read(4).unpack1('N')
-      num_cols = file.read(4).unpack1('N')
-      image_size = num_rows * num_cols
+      num_images = file.read(4).unpack1('N').to_i
+      num_rows = file.read(4).unpack1('N').to_i
+      num_cols = file.read(4).unpack1('N').to_i
 
       images = []
       num_images.times do
-        image = file.read(image_size).unpack('C*')
-        images << image
+        image = file.read(num_rows * num_cols).unpack('C*').map(&:to_i)
+        rows = image.each_slice(num_cols).to_a
+        unless num_rows == rows.size
+          raise "Image load failed. Expected #{num_rows} rows, got #{rows.size} rows after split by #{num_cols}."
+        end
+
+        matrix = Matrix.rows(rows)
+        images << matrix
       end
 
       raise "Images load failed. Expected #{num_images} images, got #{images.size}." unless num_images == images.size
@@ -39,8 +46,8 @@ module Mnist
       magic_number = file.read(4).unpack1('N')
       raise ArgumentError, "File #{idx_file} is not a label file." unless magic_number == LABEL_FILE_MAGIC_NUMBER
 
-      num_labels = file.read(4).unpack1('N')
-      labels = file.read(num_labels).unpack('C*')
+      num_labels = file.read(4).unpack1('N').to_i
+      labels = file.read(num_labels).unpack('C*').map(&:to_i)
 
       raise "Labels load failed. Expected #{num_labels} labels, got #{labels.size}." unless num_labels == labels.size
 
