c33bf7ad0aaa44c0b0832c2340988f082d246460
Hou Yuanjie <houyuanjie555@qq.com>
2024-12-23 01:07:33 +0800

    refactor(Matrix): 重构 slice 方法返回新的 Matrix 实例

--- matrix/slice.rb
+++ matrix/slice.rb
@@ -2,6 +2,7 @@ require 'matrix'
 
 class Matrix
   def slice(row_start:, row_length:, col_start:, col_length:)
-    rows.slice(row_start, row_length).map { |row| row.slice(col_start, col_length) }
+    sliced_rows = rows.slice(row_start, row_length).map { |row| row.slice(col_start, col_length) }
+    Matrix.rows(sliced_rows)
   end
 end
