ce8871e424bf9321c17464c87418019aef1e0079
Hou Yuanjie <houyuanjie555@qq.com>
2024-12-23 23:07:32 +0800

    perf(matrix): 矩阵切片时不拷贝 (copy = false)

--- matrix/slice.rb
+++ matrix/slice.rb
@@ -3,6 +3,6 @@ require 'matrix'
 class Matrix
   def slice(row_start:, row_length:, col_start:, col_length:)
     sliced_rows = rows.slice(row_start, row_length).map { |row| row.slice(col_start, col_length) }
-    Matrix.rows(sliced_rows)
+    Matrix.rows(sliced_rows, false)
   end
 end
